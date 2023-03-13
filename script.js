7e855b4b4fd55747f6901a3dcaae4da9bb7c6fbf
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-03-13 03:09:19 -0500

    made basic function for basic maths

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2){
+    num1 = prompt("Select first number")
+    num2 = prompt("Select second number")
+    return parseInt(num1 + num2)
+}
+
+function subtract(num1, num2){
+    num1 = prompt("Select first number")
+    num2 = prompt("Select second number")
+    return parseInt(num1 - num2)
+}
+
+function multiply(num1, num2){
+    num1 = prompt("Select first number")
+    num2 = prompt("Select second number")
+    return parseInt(num1 * num2)
+}
+
+function divide(num1, num2){
+    num1 = prompt("Select number to be divide by")
+    num2 = prompt("Select divider")
+    return parseInt(num1 / num2)
+}
